5a070dc154c2c5738756e477a2ac257b2bc32f65
GnomedDev <david2005thomas@gmail.com>
2024-04-23 21:44:23 +0100

    Add TypeSize implementations for Atomics

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,6 +1,12 @@
-use core::num::{
-    NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize, NonZeroU128,
-    NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize,
+use core::{
+    num::{
+        NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize, NonZeroU128,
+        NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize,
+    },
+    sync::atomic::{
+        AtomicI16, AtomicI32, AtomicI64, AtomicI8, AtomicIsize, AtomicU16, AtomicU32, AtomicU64,
+        AtomicU8, AtomicUsize,
+    },
 };
 
 use crate::{sizeof_impl, TypeSize};
@@ -24,6 +30,8 @@ sizeof_impl!(
     core::time::Duration,
     u8, u16, u32, u64, u128, usize,
     i8, i16, i32, i64, i128, isize,
+    AtomicU8, AtomicU16, AtomicU32, AtomicU64, AtomicUsize,
+    AtomicI8, AtomicI16, AtomicI32, AtomicI64, AtomicIsize,
     NonZeroU8, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU128, NonZeroUsize,
     NonZeroI8, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI128, NonZeroIsize
 );
