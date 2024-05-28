ea2bc414e24f0a4a2ebc54e5612907b78f91ff9f
GnomedDev <david2005thomas@gmail.com>
2024-05-28 11:57:13 +0100

    Remove OnceCell implementation as breaking MSRV

--- src/cell.rs
+++ src/cell.rs
@@ -31,19 +31,3 @@ impl<T: TypeSize> TypeSize for core::cell::RefCell<T> {
         }
     }
 }
-
-impl<T: TypeSize> TypeSize for core::cell::OnceCell<T> {
-    fn extra_size(&self) -> usize {
-        self.get().map_or(0, TypeSize::extra_size)
-    }
-
-    if_typesize_details! {
-        fn get_collection_item_count(&self) -> Option<usize> {
-            self.get().and_then(TypeSize::get_collection_item_count)
-        }
-
-        fn get_size_details(&self) -> alloc::vec::Vec<crate::Field> {
-            self.get().map_or(alloc::vec::Vec::new(), TypeSize::get_size_details)
-        }
-    }
-}
