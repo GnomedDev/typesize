dffcfd10598a6168bb2c708bd4f31be004dac8e6
aecsocket <43144841+aecsocket@users.noreply.github.com>
2024-10-21 15:08:50 +0100

    Add support for `Wrapping` and `Saturating` (#3)
    * Add support for Wrapping and Saturating
    
    * Feature gate `Saturating` impls
    
    * Fix clippy
    
    * Address review comments

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,7 +1,7 @@
 use core::{
     num::{
         NonZeroI128, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI8, NonZeroIsize, NonZeroU128,
-        NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize,
+        NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize, Wrapping,
     },
     sync::atomic::{
         AtomicBool, AtomicI16, AtomicI32, AtomicI64, AtomicI8, AtomicIsize, AtomicU16, AtomicU32,
@@ -32,5 +32,17 @@ sizeof_impl!(
     AtomicU8, AtomicU16, AtomicU32, AtomicU64, AtomicUsize,
     AtomicI8, AtomicI16, AtomicI32, AtomicI64, AtomicIsize,
     NonZeroU8, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU128, NonZeroUsize,
-    NonZeroI8, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI128, NonZeroIsize
+    NonZeroI8, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroI128, NonZeroIsize,
+    Wrapping<u8>, Wrapping<u16>, Wrapping<u32>, Wrapping<u64>, Wrapping<u128>, Wrapping<usize>,
+    Wrapping<i8>, Wrapping<i16>, Wrapping<i32>, Wrapping<i64>, Wrapping<i128>, Wrapping<isize>
+);
+
+#[cfg(feature = "saturating_impls")]
+use core::num::Saturating;
+
+#[cfg(feature = "saturating_impls")]
+#[rustfmt::skip]
+sizeof_impl!(
+    Saturating<u8>, Saturating<u16>, Saturating<u32>, Saturating<u64>, Saturating<u128>, Saturating<usize>,
+    Saturating<i8>, Saturating<i16>, Saturating<i32>, Saturating<i64>, Saturating<i128>, Saturating<isize>
 );
